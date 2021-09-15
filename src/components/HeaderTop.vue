1cb92739c4129e10cba8429e13c049482ceb861f
Giorgio Andrei <cipuandrei@libero.it>
2021-09-15 17:26:24 +0200

    header-top-created-and-logo-added

---
+++ src/components/HeaderTop.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div><img :src="logo" alt="EduPrime-logo" /></div>
+    <nav></nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderTop",
+  data() {
+    return {
+      logo: require("../assets/img/theme_eduprime_logo.png"),
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
