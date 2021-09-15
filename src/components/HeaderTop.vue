5eeed0d84287a6f6b06c53740f6f7c0e17625e13
Giorgio Andrei <cipuandrei@libero.it>
2021-09-15 19:08:17 +0200

    header-top-done

--- src/components/HeaderTop.vue
+++ src/components/HeaderTop.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="header-top" class="row">
-    <div class="img-box h-100 col-5 clickable">
+    <div class="img-box h-100 col-3 clickable">
       <img class="h-100" :src="logo" alt="EduPrime-logo" />
     </div>
-    <nav class="text-white d-flex col-7">
-      <ul class="d-flex align-items-center m-0">
+    <nav class="text-white d-flex col-9 justify-content-end">
+      <ul class="d-flex align-items-center my-0 mx-2">
         <li
           class="fw-bold"
           v-for="(element, index) in navElements"
@@ -22,6 +22,18 @@
           ></a>
         </li>
       </ul>
+      <a
+        href="#"
+        class="
+          big-button
+          d-flex
+          align-items-center
+          bg-yellow
+          text-white text-uppercase
+          fw-bold
+        "
+        >View courses</a
+      >
     </nav>
   </div>
 </template>
@@ -52,5 +64,9 @@ export default {
 }
 .color-yellow {
   color: $both-yellow;
+}
+
+.bg-yellow {
+  background-color: $both-yellow;
 }
 </style>
