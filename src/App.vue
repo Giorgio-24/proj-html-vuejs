42c478416158fddb3bc94c57b5feb0e7b74a4e69
Giorgio Andrei <cipuandrei@libero.it>
2021-09-15 17:25:42 +0200

    header-top-and-bottom-added

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,27 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <header>
+      <HeaderTop />
+      <HeaderBottom />
+    </header>
+    <main></main>
+    <footer></footer>
+  </div>
 </template>
 
 <script>
-/* import HelloWorld from './components/HelloWorld.vue' */
+import HeaderTop from "./components/HeaderTop.vue";
+import HeaderBottom from "./components/HeaderBottom.vue";
 
 export default {
   name: "App",
-  components: {},
+  components: {
+    HeaderTop,
+    HeaderBottom,
+  },
 };
 </script>
 
 <style lang="scss">
+@import "assets/scss/_style.scss";
 </style>
