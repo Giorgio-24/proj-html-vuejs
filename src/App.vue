ecd84657651f142b528beed02d55297c38e536fa
Giorgio Andrei <cipuandrei@libero.it>
2021-09-16 11:03:15 +0200

    main-header-done

--- src/App.vue
+++ src/App.vue
@@ -6,20 +6,28 @@
         <HeaderBottom />
       </div>
     </header>
-    <main></main>
-    <footer></footer>
+    <main class="pt-5">
+      <div class="container">
+        <MainHeader class="row" />
+      </div>
+    </main>
+    <footer class="pt-5">
+      <div class="container"></div>
+    </footer>
   </div>
 </template>
 
 <script>
 import HeaderTop from "./components/HeaderTop.vue";
 import HeaderBottom from "./components/HeaderBottom.vue";
+import MainHeader from "./components/MainHeader.vue";
 
 export default {
   name: "App",
   components: {
     HeaderTop,
     HeaderBottom,
+    MainHeader,
   },
 };
 </script>
---
+++ src/components/MainHeader.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="mt-5">
+    <Paragraph
+      :icon="icon"
+      :title="title"
+      :paragraph="paragraph"
+      :buttonText="buttonText"
+      :buttonColor="buttonColor"
+    />
+    <div class="mt-4 col-5 offset-1">
+      <img class="img-fluid" :src="illustration" alt="" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Paragraph from "./Paragraph.vue";
+export default {
+  name: "MainHeader",
+  components: {
+    Paragraph,
+  },
+  data() {
+    return {
+      icon: require("../assets/img/Books-icon.png"),
+      title: "Thousands of courses for any type of student",
+      paragraph:
+        "At EduPrime, it doesn't matter what domanin you wish to pursue a career in. Here you can find a course that satisfies your needs within a click away and applies for a course in a matter of minutes. EduPrime is ranked as the most versatile university in the world, thanks to the number of courses it provides.",
+      buttonText: "Browse through courses",
+      buttonColor: "bg-yellow",
+      illustration: require("../assets/img/Graduation-Illustration.png"),
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
