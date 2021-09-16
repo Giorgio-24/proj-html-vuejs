09e6923af1371aee199d4bb30a6f7757221d08b1
Giorgio Andrei <cipuandrei@libero.it>
2021-09-16 11:45:42 +0200

    faculties-component-done

--- src/components/Faculties.vue
+++ src/components/Faculties.vue
@@ -1,5 +1,13 @@
 <template>
-  <section></section>
+  <section>
+    <h2 class="col-12 font-35 text-center font-35 mb-4">
+      Faculties avaiable at EduPrime
+    </h2>
+    <div class="col-4 offset-4 text-center">
+      A single university with a load of courses, tailored to satisfy any
+      student's needs.
+    </div>
+  </section>
 </template>
 
 <script>
