577ab14d007aa5f66a2dfc2c941202fcda113fa1
Giorgio Andrei <cipuandrei@libero.it>
2021-09-17 11:13:09 +0200

    partnersLogos-component-done

---
+++ src/components/PartnersLogos.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="h-100 d-flex justify-content-center" :class="column">
+    <img class="img-fluid" :class="imgClass" :src="image" alt="partner-logo" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PartnersLogos",
+  props: ["column", "image", "imgClass"],
+};
+</script>
+
+<style lang="scss" scoped>
+.max-dimentions {
+  max-width: 58px;
+  max-height: 43px;
+}
+</style>
