116762ce4a11455e4bac24029109514d165fa65e
Giorgio Andrei <cipuandrei@libero.it>
2021-09-17 10:13:29 +0200

    InputPill-component-done

---
+++ src/components/InputPill.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="d-flex">
+    <input
+      class="border-0 p-4 rounded-pill"
+      :class="columnClass"
+      :type="type"
+      :placeholder="placeholder"
+    />
+    <div
+      class="icon-button rounded-circle text-white position-relative"
+      :class="buttonColor"
+    >
+      <i class="fa-2x p-4 rounded-circle" :class="buttonIcon"></i>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "InputPill",
+  props: ["type", "placeholder", "buttonColor", "buttonIcon", "columnClass"],
+};
+</script>
+
+<style lang="scss" scoped>
+input {
+  outline: none;
+}
+.icon-button {
+  right: 75px;
+}
+</style>
